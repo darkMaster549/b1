74812e849041475f9298f335b511d110301bfa28
Casuke <kylellamas723@gmail.com>
2026-04-18 13:07:56 +0800

    Update Header.lua

--- Vm/Resources/Templates/Header.lua
+++ Vm/Resources/Templates/Header.lua
@@ -1,13 +1,13 @@
+-- // fix constant being empty
+
 -- Header Template
 return [=[
--- Vars
-local char,byte,sub,ttostring,pcall,unpack,concat,tonumber,setmeta,__metatable,__index,proxy,pnt,Constants,pairs,__newindex,next,dot,gsub,stringv,find,tfind = Env["string"]["char"],Env["string"]["byte"],Env["string"]["sub"],Env["tostring"],Env["pcall"],Env["table"]["unpack"] or Env["unpack"],Env["table"]["concat"],Env["tonumber"],Env["setmetatable"],"__metatable","__index",Env["newproxy"],Env["print"],{CONSTANTS_HERE_BASEVM},Env["pairs"],"__newindex",Env["next"],".",Env["string"]["gsub"],"string",Env["string"]["find"],function(targetTable, value)
+local char,byte,sub,ttostring,pcall,unpack,concat,tonumber,setmeta,__metatable,__index,proxy,pnt,Constants,pairs,__newindex,next,dot,gsub,stringv,find,tfind = Env["string"]["char"],Env["string"]["byte"],Env["string"]["sub"],Env["tostring"],Env["pcall"],Env["table"]["unpack"] or Env["unpack"],Env["table"]["concat"],Env["tonumber"],Env["setmetatable"],"__metatable","__index",Env["newproxy"],Env["print"],__constants,Env["pairs"],"__newindex",Env["next"],".",Env["string"]["gsub"],"string",Env["string"]["find"],function(targetTable, value)
 	for i,v in pairs(targetTable) do
 		if v == value then
 			return i
 		end
 	end
-	
 	return nil
 end
 ]=]
