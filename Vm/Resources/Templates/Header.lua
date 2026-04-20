d4189e619ec7ba36b2c5968450855e486ec6962c
Casuke <kylellamas723@gmail.com>
2026-04-20 10:05:42 +0800

    Update Header.lua

--- Vm/Resources/Templates/Header.lua
+++ Vm/Resources/Templates/Header.lua
@@ -1,6 +1,5 @@
--- // fix constant being empty
+-- // fix constant being empty and always nil
 
--- Header Template
 return [=[
 local char,byte,sub,ttostring,pcall,unpack,concat,tonumber,setmeta,__metatable,__index,proxy,pnt,Constants,pairs,__newindex,next,dot,gsub,stringv,find,tfind = Env["string"]["char"],Env["string"]["byte"],Env["string"]["sub"],Env["tostring"],Env["pcall"],Env["table"]["unpack"] or Env["unpack"],Env["table"]["concat"],Env["tonumber"],Env["setmetatable"],"__metatable","__index",Env["newproxy"],Env["print"],__constants,Env["pairs"],"__newindex",Env["next"],".",Env["string"]["gsub"],"string",Env["string"]["find"],function(targetTable, value)
 	for i,v in pairs(targetTable) do
@@ -10,4 +9,5 @@ local char,byte,sub,ttostring,pcall,unpack,concat,tonumber,setmeta,__metatable,_
 	end
 	return nil
 end
+local C = __constants
 ]=]
