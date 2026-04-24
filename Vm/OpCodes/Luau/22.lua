a323aa901e36e16f52da78fcd7887662f95973ad
Casuke <kylellamas723@gmail.com>
2026-04-24 13:28:27 +0800

    Create 22.lua

---
+++ Vm/OpCodes/Luau/22.lua
@@ -0,0 +1,5 @@
+-- JUMPBACK: pc += E (backward jump)
+return function(inst, shiftAmount, constant, settings)
+    local e = inst.E or inst.sBx or 0
+    return ("pointer = pointer + %d"):format(e)
+end
