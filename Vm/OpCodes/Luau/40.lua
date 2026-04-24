c5445ad962d9ff0cd37f4920dfbdbb1971ea0c00
Casuke <kylellamas723@gmail.com>
2026-04-24 13:33:30 +0800

    Create 40.lua

---
+++ Vm/OpCodes/Luau/40.lua
@@ -0,0 +1,5 @@
+-- DIVK: R(A) := R(B) / K(C)
+return function(inst, s, c, settings)
+    local a = _G.getReg(inst,"A"); local b = _G.getReg(inst,"B"); local ci = _G.getReg(inst,"C")
+    return ("\tStack[%d] = Stack[%d] / C[%d]"):format(a, b, _G.getMappedConstant(ci))
+end
