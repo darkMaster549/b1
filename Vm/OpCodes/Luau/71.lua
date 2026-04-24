84120b8e2313fd8f4296c047f8619c16bd2c629b
Casuke <kylellamas723@gmail.com>
2026-04-24 13:41:05 +0800

    Create 71.lua

---
+++ Vm/OpCodes/Luau/71.lua
@@ -0,0 +1,5 @@
+-- DIVRK: R(A) := K(B) / R(C)
+return function(inst, s, c, settings)
+    local a = _G.getReg(inst,"A"); local bi = _G.getReg(inst,"B"); local c2 = _G.getReg(inst,"C")
+    return ("\tStack[%d] = C[%d] / Stack[%d]"):format(a, _G.getMappedConstant(bi), c2)
+end
