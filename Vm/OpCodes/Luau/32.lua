c658807f9f02dff7882e8c4887c9ce30c10958c6
Casuke <kylellamas723@gmail.com>
2026-04-24 13:30:57 +0800

    Create 32.lua

---
+++ Vm/OpCodes/Luau/32.lua
@@ -0,0 +1,5 @@
+-- SUB: R(A) := R(B) - R(C)
+return function(inst, s, c, settings)
+    local a = _G.getReg(inst,"A"); local b = _G.getReg(inst,"B"); local c2 = _G.getReg(inst,"C")
+    return ("\tStack[%d] = Stack[%d] - Stack[%d]"):format(a,b,c2)
+end
