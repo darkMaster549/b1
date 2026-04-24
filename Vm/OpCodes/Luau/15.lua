35785efa6ea20e4f1a9eb08a7386219200f36b81
Casuke <kylellamas723@gmail.com>
2026-04-24 13:26:13 +0800

    Create 15.lua

---
+++ Vm/OpCodes/Luau/15.lua
@@ -0,0 +1,7 @@
+-- GETTABLEN: R(A) := R(B)[C] integer index
+return function(inst, shiftAmount, constant, settings)
+    local reg_a = _G.getReg(inst, "A")
+    local reg_b = _G.getReg(inst, "B")
+    local reg_c = _G.getReg(inst, "C")
+    return ("\tStack[%d] = Stack[%d][%d]"):format(reg_a, reg_b, reg_c)
+end
