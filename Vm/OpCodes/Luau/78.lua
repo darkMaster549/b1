11235b57336ea6d4b61dbde0918f199fe184d9a9
Casuke <kylellamas723@gmail.com>
2026-04-24 13:43:47 +0800

    Create 78.lua

---
+++ Vm/OpCodes/Luau/78.lua
@@ -0,0 +1,12 @@
+-- JUMPXEQKN: jump if R(A) == number K(AUX)
+return function(inst, shiftAmount, constant, settings)
+    local reg_a = _G.getReg(inst, "A")
+    local d = inst.D or inst.Bx or 0
+    local aux = inst.AUX or 0
+    local mappedIdx = _G.getMappedConstant(aux % 0x1000000)
+    return ([=[
+    if Stack[%d] == C[%d] then
+        pointer = pointer + %d
+    end
+    ]=]):format(reg_a, mappedIdx, d)
+end
