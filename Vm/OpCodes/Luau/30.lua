4a9c2c2e30e13e64ab087c88274672b9e0b5b0d9
Casuke <kylellamas723@gmail.com>
2026-04-24 13:30:28 +0800

    Create 30.lua

---
+++ Vm/OpCodes/Luau/30.lua
@@ -0,0 +1,11 @@
+-- JUMPIFNOTLT: if not (R(A) < R(AUX)) then pc += D
+return function(inst, shiftAmount, constant, settings)
+    local reg_a = _G.getReg(inst, "A")
+    local d = inst.D or inst.Bx or 0
+    local aux = inst.AUX or 0
+    return ([=[
+    if not (Stack[%d] < Stack[%d]) then
+        pointer = pointer + %d
+    end
+    ]=]):format(reg_a, aux, d)
+end
