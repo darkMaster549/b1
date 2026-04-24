92a8cfb20ad25984324dc0e73cf2d03e5a14b0b9
Casuke <kylellamas723@gmail.com>
2026-04-24 13:28:41 +0800

    Create 23.lua

---
+++ Vm/OpCodes/Luau/23.lua
@@ -0,0 +1,10 @@
+-- JUMPIF: if R(A) then pc += D
+return function(inst, shiftAmount, constant, settings)
+    local reg_a = _G.getReg(inst, "A")
+    local d = inst.D or inst.Bx or 0
+    return ([=[
+    if Stack[%d] then
+        pointer = pointer + %d
+    end
+    ]=]):format(reg_a, d)
+end
