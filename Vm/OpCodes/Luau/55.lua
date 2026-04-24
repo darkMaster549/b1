7dcc2388dcdbae59d26166551845d7e03f635078
Casuke <kylellamas723@gmail.com>
2026-04-24 13:37:21 +0800

    Create 55.lua

---
+++ Vm/OpCodes/Luau/55.lua
@@ -0,0 +1,15 @@
+-- FORNLOOP: numeric for step
+return function(instruction, shiftAmount, constant, settings)
+    local reg_a = _G.getReg(instruction, "A")
+    local d = instruction.D or instruction.Bx or 0
+    return ([=[
+    local _step  = Stack[%d]
+    local _limit = Stack[%d]
+    local _index = Stack[%d] + _step
+    Stack[%d] = _index
+    if (_step > 0 and _index <= _limit) or (_step <= 0 and _index >= _limit) then
+        pointer = pointer + %d
+        Stack[%d] = _index
+    end
+    ]=]):format(reg_a+2, reg_a+1, reg_a, reg_a, d, reg_a+3)
+end
