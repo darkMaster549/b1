92f81848528644c9d61320379547a0532ae9ac11
Casuke <kylellamas723@gmail.com>
2026-04-24 13:16:21 +0800

    Update Settings.lua

--- Input/Settings.lua
+++ Input/Settings.lua
@@ -7,7 +7,8 @@ return {
 	["ControlFlowFlattening"] = false,					-- Flattens control flow for simple obfuscation hardening
 	--
 	["Debug"] = false,									-- Enables debug tools to help debug errors
-	["Minify"] = false, 								-- Minfies the output for smaller size
-    ["Watermark"] = "File generated using [Zartha]", 	-- Input any watermark you want here
+	["Minify"] = false, 								-- Minifies the output for smaller size
+    ["Watermark"] = "Hebrew", 	-- Input any watermark you want here
 	["LuaUCompatibility"] = false,						-- Removes Luau specific syntax to make it compatible with Lua 5.1 compiler
+	["LuauMode"] = false,								-- Use Luau 80+ opcode set instead of Lua 5.1 38 opcodes
 }
