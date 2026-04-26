ac3a3ccdcb1c87ae3e58b65c007d13f4782f7e51
Casuke <kylellamas723@gmail.com>
2026-04-27 00:49:29 +0800

    Add numexpr

--- Input/Settings.lua
+++ Input/Settings.lua
@@ -1,14 +1,15 @@
 -- You cannot directly edit these and you must use flags in Main.lua to change these settings
 
 return {
-	["ConstantProtection"] = false,						-- Encrypts constants for stronger constant security
-	["EncryptStrings"] = false, 						-- Encrypts strings in output for stronger security
-	["AntiTamper"] = false,								-- Injects anti-tamper checks to detect script modification
-	["ControlFlowFlattening"] = false,					-- Flattens control flow for simple obfuscation hardening
+	["ConstantProtection"]    = false,			-- Encrypts constants for stronger constant security
+	["EncryptStrings"]        = false, 			-- Encrypts strings in output for stronger security
+	["AntiTamper"]            = false,			-- Injects anti-tamper checks to detect script modification
+	["ControlFlowFlattening"] = false,			-- Flattens control flow for simple obfuscation hardening
+	["NumberToExpressions"]   = false,			-- Replaces integer literals with equivalent math expressions
 	--
-	["Debug"] = false,									-- Enables debug tools to help debug errors
-	["Minify"] = false, 								-- Minifies the output for smaller size
-    ["Watermark"] = "Hebrew", 	-- Input any watermark you want here
-	["LuaUCompatibility"] = false,						-- Removes Luau specific syntax to make it compatible with Lua 5.1 compiler
-	["LuauMode"] = false,								-- Use Luau 80+ opcode set instead of Lua 5.1 38 opcodes
+	["Debug"]                 = false,			-- Enables debug tools to help debug errors
+	["Minify"]                = false, 			-- Minifies the output for smaller size
+	["Watermark"]             = "Hebrew", 		-- Input any watermark you want here
+	["LuaUCompatibility"]     = false,			-- Removes Luau specific syntax to make it compatible with Lua 5.1 compiler
+	["LuauMode"]              = false,			-- Use Luau 80+ opcode set instead of Lua 5.1 38 opcodes
 }
