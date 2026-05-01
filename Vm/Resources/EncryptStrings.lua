50f9d66dd32444b4727e36b9218cef78d50987c9
Casuke <kylellamas723@gmail.com>
2026-05-01 23:05:43 +0800

    Update EncryptStrings.lua

--- Vm/Resources/EncryptStrings.lua
+++ Vm/Resources/EncryptStrings.lua
@@ -13,9 +13,7 @@ local function xorBit(a, b)
     return r
 end
 
-local BASE91 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#$%&()*+,-./:;<=>?@[]^_`{|}~"
-
-local function base91Encode(input, salt)
+local function base10Encode(input, salt)
     if not input or #input == 0 then return "~" end
     local src = {}
     for i = 1, #input do src[i] = string.byte(input, i) end
@@ -27,31 +25,8 @@ local function base91Encode(input, salt)
         transformed[i] = xorBit(b, prev % 256)
     end
     local out = {}
-    local v, b = 0, 1
     for i = 1, #transformed do
-        v = v + transformed[i] * b
-        b = b * 256
-        if b > 8191 then
-            local val = v % 8192
-            if val > 88 then
-                out[#out+1] = BASE91:sub((val%91)+1, (val%91)+1)
-                out[#out+1] = BASE91:sub(math.floor(val/91)+1, math.floor(val/91)+1)
-                v = math.floor(v / 8192)
-                b = math.floor(b / 8192)
-            else
-                val = v % 16384
-                out[#out+1] = BASE91:sub((val%91)+1, (val%91)+1)
-                out[#out+1] = BASE91:sub(math.floor(val/91)+1, math.floor(val/91)+1)
-                v = math.floor(v / 16384)
-                b = math.floor(b / 16384)
-            end
-        end
-    end
-    if b > 1 then
-        out[#out+1] = BASE91:sub((v%91)+1, (v%91)+1)
-        if b > 90 or v > 90 then
-            out[#out+1] = BASE91:sub(math.floor(v/91)+1, math.floor(v/91)+1)
-        end
+        out[i] = string.format("%03d", transformed[i])
     end
     local result = table.concat(out)
     return (result == "" and "~" or result)
@@ -60,14 +35,14 @@ end
 return function(scriptSource, wantsFunction)
     if wantsFunction == true then
         return function(str, salt)
-            local encoded = base91Encode(str, salt)
+            local encoded = base10Encode(str, salt)
             return encoded, encoded
         end
     end
 
     local encryptedScript = scriptSource:gsub('"(.-)"', function(match)
         local salt = math.random(100, 9999)
-        local encoded = base91Encode(match, salt)
+        local encoded = base10Encode(match, salt)
         encoded = encoded:gsub("\\", "\\\\"):gsub('"', '\\"')
         return string.format('__decrypt_fn("%s",%d)', encoded, salt)
     end)
